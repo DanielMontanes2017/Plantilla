c7698d0113886fe102a716ea927d3e41ce39cac0
DanielMontanes2017 <edoras995@hotmail.com>
2017-01-22 21:36:58 +0100

    Plantilla
    Plantilla personalitzada correctament

--- Plantilla/Plantilla.ino
+++ Plantilla/Plantilla.ino
@@ -20,21 +20,3 @@ void loop()
 }
 
 //********************************* Funcions **************************************
-
-
-
-
-
-
-
-
-
-void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
